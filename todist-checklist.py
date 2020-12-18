f35a51d5dc78d628fafa011ecf4e786fbf653a9a
justcop <justcop@gmail.com>
2020-12-18 23:48:42 +0000

    Update todist-checklist.py

--- todist-checklist.py
+++ todist-checklist.py
@@ -4,6 +4,9 @@ import todoist
 from todoist.api import TodoistAPI
 from configs import api_token
 
+f = requests.get(https://docs.google.com/spreadsheets/d/1xzHvUuTLteQDt0mgByuj7Zypnv-7LUQmPP2Ij-mq5IY/gviz/tq?tqx=out:csv)
+print(str(f))
+
 api = TodoistAPI(api_token)
 api.sync()
 try:
@@ -14,6 +17,6 @@ except:
 project = api.projects.add('Checklists')
 project.update(color='31')
 api.commit()
-api.templates.import_into_project(project["id"], 'https://docs.google.com/spreadsheets/d/1xzHvUuTLteQDt0mgByuj7Zypnv-7LUQmPP2Ij-mq5IY/gviz/tq?tqx=out:csv')
+api.templates.import_into_project(project["id"], '')
 with open('projectid', 'w') as f:
        f.write(str(project["id"]))
