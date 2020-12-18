bb00e4a32cfcbbcbddb86404712d118fc59b824a
justcop <justcop@gmail.com>
2020-12-18 23:23:26 +0000

    Update todist-checklist.py

--- todist-checklist.py
+++ todist-checklist.py
@@ -1,6 +1,4 @@
 #! /usr/bin/env python3
-
-
 import requests, json
 import todoist
 from todoist.api import TodoistAPI
@@ -11,9 +9,10 @@ api.sync()
 try:
        projectid = open("projectid", "r")
        requests.delete("https://api.todoist.com/rest/v1/projects/" + str(projectid.read()), headers={"Authorization": "Bearer " + api_token})
-project = api.projects.add('Checklists')
-project.update(color='31')
-api.commit()
-api.templates.import_into_project(project["id"], '/share/Checklist.csv')
-with open('projectid', 'w') as f:
-       f.write(str(project["id"]))
+finally:
+       project = api.projects.add('Checklists')
+       project.update(color='31')
+       api.commit()
+       api.templates.import_into_project(project["id"], '/share/Checklist.csv')
+       with open('projectid', 'w') as f:
+              f.write(str(project["id"]))
