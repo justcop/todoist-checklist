da632bed4d33cdf3155f5f387fa0f1743739cb0b
justcop <justcop@gmail.com>
2020-12-18 21:36:58 +0000

    Create todist-checklist.py

---
+++ todist-checklist.py
@@ -0,0 +1,9 @@
+import todoist
+api = todoist.TodoistAPI('5010e354fbb1e86dd6fff9f7ef9880324ce40989')
+project = api.projects.get_by_id(checklist)
+project.delete()
+api.commit()
+project = api.projects.add('checklist')
+api.commit()
+api.templates.import_into_project(128501470, '/share/Checklist.csv')
+print(todoist.api.json_dumps(api)
