dcca97af5a4a49fa110b700ecbc33836afe199ba
justcop <justcop@gmail.com>
2020-12-18 21:50:25 +0000

    Update todist-checklist.py

--- todist-checklist.py
+++ todist-checklist.py
@@ -1,5 +1,5 @@
 import todoist
-from todoist import TodoistAPI
+from todoist.api import TodoistAPI
 api = todoist.TodoistAPI('5010e354fbb1e86dd6fff9f7ef9880324ce40989')
 project = api.projects.get_by_id(checklist)
 project.delete()
