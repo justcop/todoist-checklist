979661fe17415dc924e725c705701baf691eebc0
justcop <justcop@gmail.com>
2021-01-29 13:42:13 +0000

    Update todoist-checklist.py

--- todoist-checklist.py
+++ todoist-checklist.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import requests, json, todoist
 from todoist.api import TodoistAPI
-from configs import todoist_token, gsheets_ID, gsheets_GID
+from configs import todoist_token, gsheets_ID, gsheets_GID, parentid
 
 res=requests.get(url='https://docs.google.com/spreadsheets/d/' + gsheets_ID + '/export?format=csv&id=' + gsheets_ID + '&gid=' + gsheets_GID)
 open('checklist.csv', 'wb').write(res.content)
@@ -18,6 +18,9 @@ try:
 except:
        print('Unable to delete existing project')
 
+if parentid
+project = api.projects.add('Checklists', color=31, parent_id=parentid)
+else
 project = api.projects.add('Checklists', color=31)
 api.commit()
 print('Adding new replacement project')
